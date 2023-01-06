ae162e8ef967dcbf1853d1074d7633f1779ae6f5
Zemogiter <zumi999@vp.pl>
2023-01-06 21:00:25 +0100

    Initial port to BepInEx

---
+++ PDARadioBepInEx.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Story;
+using UnityEngine;
+using BepInEx;
+
+namespace PDARadioBepInEx
+{
+
+    [BepInPlugin("com.GruffCassquatch.PDARadioBepInEx", "PDARadioBepInEx", "1.0.0.0")]
+
+    public class PDARadioBepInEx : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("com.GruffCassquatch.PDARadioBepInEx");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    [HarmonyPatch(typeof(StoryGoalManager))]
+    [HarmonyPatch("InvokePendingMessageEvent")]
+    internal class StoryGoalManager_InvokePendingMessageEvent_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix()
+        {
+            if (GameObject.Find("EscapePod").transform.Find("ModulesRoot/RadioRoot/Radio(Clone)").gameObject.GetComponent<LiveMixin>().IsFullHealth())
+            {
+                StoryGoalManager.main.Invoke("ExecutePendingRadioMessage", 0f);
+            }
+        }
+    }
+
+}
